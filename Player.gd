4dc662bd41d0643483dcda4307255513761a123d
Eric Schubert <eric.schubert.3d@gmail.com>
2022-01-19 20:31:33 +0100

    Added background, added spaceship acceleration, added exhaust animation

--- Player.gd
+++ Player.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-const acc_force = .05
-const rot_force = .05
+const acc_multi = .05
+const rot_multi = .05
 
 var dir = Vector2(0.0, 1.0)
+var force = Vector2(0.0, 0.0)
 var rot = 0
 var pos = Vector2(0.0, 0.0)
 var speed = 0
+var exhaustScale = 0
+
+var exhaust
 
 func _ready():
+	exhaust = get_node("Exhaust")
 	set_process(true)
 
 
@@ -16,23 +21,31 @@ func _process(delta):
 	pos = self.position
 	rot = self.rotation
 	
-
+	var exhaustGoal = 0.0
+	dir = Vector2(cos(rot - 3.14/2), sin(rot - 3.14/2))
+	
 	
 	
 	if(Input.is_action_pressed("MovementAccelerate")):
 		print("accelerate")
-		
+		force+= dir * acc_multi
+		exhaustGoal = 1.0
+	else:
+		exhaustGoal = 0.0
 	
 	if(Input.is_action_pressed("MovementLeft")):
 		print("left")
-		rot-=1 * rot_force
+		rot-=1 * rot_multi
 	
 	if(Input.is_action_pressed("MovementRight")):
 		print("right")
-		rot+=1 * rot_force
+		rot+=1 * rot_multi
 		
-	self.position+= dir * speed
+	self.position+= force
 	self.rotation = rot
 	
-	print(pos)
-	print(rot)
+	exhaustScale = lerp(exhaustScale, exhaustGoal, delta * 10.0)
+	
+	exhaust.scale = Vector2(1.0 ,exhaustScale)
+	
+	print(dir)
