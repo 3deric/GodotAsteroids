bce0c8ae57c38d07315c807427f233ac5af81977
Eric Schubert <eric.schubert.3d@gmail.com>
2022-01-22 20:34:32 +0100

    Added random sprite selection for the asteroids

--- Scripts/Asteroids.gd
+++ Scripts/Asteroids.gd
@@ -1,10 +1,14 @@
 extends RigidBody2D
 
 export var maxAngularVel = 3.0
-#xport var (sprite) sprites
+export(Array, Texture) var sprites
 
 func _ready():
 	angular_damp = 0.0
+	var sprite = get_node("Sprite")
+	randomize()
+	var random = randi()%3+1
+	sprite.texture = sprites[random]
 	angular_velocity = rand_range(-maxAngularVel, maxAngularVel)
 	set_process(true)
 
